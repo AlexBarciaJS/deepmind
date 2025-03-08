ebc8895e6363c90d063116c307c30ff27abe5022
AlexBarciaJS <guillermoalexisbarcia@gmail.com>
2025-03-07 21:39:30 -0500

    fix: cors

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controller.brain import router as brain_router  # Importar el router
 
 app = FastAPI()
+
+# CORS Configuration
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:8080"],  # Allow Vue frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 # Incluir el router de brain.py
 app.include_router(brain_router, prefix="/brain", tags=["Brain"])
