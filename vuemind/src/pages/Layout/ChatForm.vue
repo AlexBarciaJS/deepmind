2aa4b70f1aaa69378746113c582d55bea1d2fe86
AlexBarciaJS <guillermoalexisbarcia@gmail.com>
2025-03-08 08:25:04 -0500

    vue feat: chat get

--- vuemind/src/pages/Layout/ChatForm.vue
+++ vuemind/src/pages/Layout/ChatForm.vue
@@ -6,11 +6,11 @@
           <div class="md-layout-item md-size-100">
             <md-field maxlength="5">
               <label>Haz una pregunta</label>
-              <md-textarea v-model="aboutme"></md-textarea>
+              <md-textarea v-model="message"></md-textarea>
             </md-field>
           </div>
           <div class="md-layout-item md-size-100 text-right">
-            <md-button class="md-button md-round md-just-icon md-twitter">
+            <md-button class="md-button md-round md-just-icon md-twitter" @click="sendQuestion">
               <i class="fa fa-paper-plane" aria-hidden="true"></i>
             </md-button>
           </div>
@@ -20,19 +20,37 @@
   </form>
 </template>
 <script>
+import axios from "axios";
+
 export default {
-  name: "chat-form",
-  props: {
-    dataBackgroundColor: {
-      type: String,
-      default: "",
-    },
-  },
   data() {
     return {
-      aboutme: "",
+      message: "",   // Stores the text entered in the textarea
+      response: ""   // Stores the API response
     };
   },
+  methods: {
+    async sendQuestion() {
+      if (!this.message.trim()) {
+        alert("Please enter a question before sending.");
+        return;
+      }
+
+      try {
+        // Build the URL with the entered message
+        const url = `http://127.0.0.1:8000/brain/preguntar/?query=${encodeURIComponent(this.message)}`;
+        
+        // Make the GET request to the API
+        const apiResponse = await axios.get(url);
+
+        // Store the API response in the state variable
+        this.response = apiResponse.data.respuesta;
+      } catch (error) {
+        console.error("Error sending the request:", error);
+        this.response = "An error occurred while fetching the response.";
+      }
+    }
+  }
 };
 </script>
 <style></style>
